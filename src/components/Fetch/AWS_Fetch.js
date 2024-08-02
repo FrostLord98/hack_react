152c91005d29a709643d2833abff86ae85ddc8e6
Luis Obando <110127391+FrostLord98@users.noreply.github.com>
2024-08-02 00:22:40 -0400

    Update AWS_Fetch.js

--- src/components/Fetch/AWS_Fetch.js
+++ src/components/Fetch/AWS_Fetch.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-  baseURL: 'https://laog98.pythonanywhere.com',
+  baseURL: 'https://hack-react-9yh5.vercel.app',
   //baseURL: 'http://127.0.0.1:5000',
   timeout: 100000
 });
