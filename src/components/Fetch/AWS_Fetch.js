5987c37a1ac0098866f8ccf8f9a4465173d42171
Luis Obando <110127391+FrostLord98@users.noreply.github.com>
2024-08-02 11:03:12 -0400

    Update AWS_Fetch.js

--- src/components/Fetch/AWS_Fetch.js
+++ src/components/Fetch/AWS_Fetch.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-  baseURL: 'https://react-backend-git-main-laog98s-projects.vercel.app',
+  //baseURL: 'https://react-backend-git-main-laog98s-projects.vercel.app',
+  baseURL: 'https://react-drab-nine.vercel.app',
   timeout: 100000
 });
 
